5b7fe20917cfce98dd13fec41649e43a3b7e585f
Edward Shin <edshin7@gmail.com>
2018-09-13 16:31:58 -0700

    Copy with url api

--- eat/FoodVC.swift
+++ eat/FoodVC.swift
@@ -26,6 +26,31 @@ class FoodVC: UIViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
+    func getRestaurant() {
+        // specify the url that we will be sending the GET request to
+        let url = URL(string: "http://swapi.co/api/people")
+        // create a URLSession to handle the request tasks
+        let session = URLSession.shared
+        // create a "data task" to make the request and run completion handler
+        let task = session.dataTask(with: url!, completionHandler: {
+            // see: Swift closure expression syntax
+            data, response, error in
+            // data -> JSON data, response -> headers and other meta-information, error-> if one occurred
+            // "do-try-catch" blocks execute a try statement and then use the catch statement for errors
+            do {
+                // try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
+                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+                    print(jsonResult)
+                }
+            } catch {
+                print(error)
+            }
+        })
+        // execute the task and then wait for the response
+        // to run the completion handler. This is async!
+        task.resume()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
